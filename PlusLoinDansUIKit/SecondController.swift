71128147139de68a3d4cdf814459d0dbe6ca1d14
hakan <hakan.kt@outlook.fr>
2022-07-12 09:07:55 +0200

    first commit

---
+++ PlusLoinDansUIKit/SecondController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondController.swift
+//  PlusLoinDansUIKit
+//
+//  Created by kenter on 12/07/2022.
+//
+
+import UIKit
+
+class SecondController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Second View")
+    }
+}
--- PlusLoinDansUIKit/ViewController.swift
+++ PlusLoinDansUIKit/ViewController.swift
@@ -9,9 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        print("Codeur")
+    }
+    
+    override func loadView() {
+        super.loadView()
+        print("Load View ")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("View Did Load")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("Will A")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("Did A")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("Will D")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("Did D")
     }
 
 
